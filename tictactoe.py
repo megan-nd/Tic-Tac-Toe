055def84fe02b66e79f7262b3be2905f56ed4845
Megan Baig <git@megs.bulldy80.com>
2015-07-11 17:23:29 -0400

    changed row and column to start from 0; added get_item and set_item

--- tictactoe.py
+++ tictactoe.py
@@ -30,8 +30,8 @@ def vertical_line(x):
 class Cursor:
     def __init__(self,canvas,color):
         self.id = canvas.create_rectangle(20,20,40,40, fill=color, outline='white')
-        self.row = 2
-        self.column = 2
+        self.row = 1
+        self.column = 1
         self.width = segment_length-2
         self.height = segment_length-2
         self.canvas = canvas
@@ -48,49 +48,72 @@ class Cursor:
         tk.update()
 
     def move_up(self,evt):
-        if self.row == 1:
+        if self.row == 0:
             return
         self.row = self.row-1
         self.draw()
 
     def move_down(self,evt):
-        if self.row == 3:
+        if self.row == 2:
             return
         self.row = self.row+1
         self.draw()
 
     def move_left(self,evt):
-        if self.column == 1:
+        if self.column == 0:
             return
         self.column = self.column-1
         self.draw()
 
     def move_right(self,evt):
-        if self.column == 3:
+        if self.column == 2:
             return
         self.column = self.column+1
         self.draw()
 
     def get_x(self):
-        x = x1+(self.column-1)*segment_length+(0.5*segment_length)-(self.width/2)
+        x = x1+self.column*segment_length+(0.5*segment_length)-(self.width/2)
         return x
 
     def get_y(self):
-        y = y1+(self.row-1)*segment_length+(0.5*segment_length)-(self.height/2)
+        y = y1+self.row*segment_length+(0.5*segment_length)-(self.height/2)
         return y
 
 class XO:
     def __init__ (self,canvas,cursor,color,character):
         x = cursor.get_x()+cursor.width/2
         y = cursor.get_y()+cursor.height/2
-        canvas.create_text(x,y,text=character,fill=color,font=('Courier',50))
-        
+        canvas.create_text(x,y,text=character,fill=color,font=('Courier',60))        
         
 def button1_pressed(evt):
+    if get_item(cursor.row,cursor.column) != None:
+        return
     X = XO(canvas,cursor,'red','X')
+    set_item(cursor.row,cursor.column,X)
 
 def button2_pressed(evt):
+    if get_item(cursor.row,cursor.column) != None:
+        return
     O = XO(canvas,cursor,'blue','O')
+    set_item(cursor.row,cursor.column,O)
+
+def print_item(row,column):
+    print(items[row][column])
+
+def get_item(row,column):
+    return items[row][column]
+
+def set_item(row,column,item):
+    items[row][column] = item
+
+def print_items(evt):
+    print(items)
+
+items = [
+    [None,None,None],
+    [None,None,None],
+    [None,None,None],
+    ]
 
 x1,x2 = get_x1_x2()
 y1,y2 = get_y1_y2()
@@ -105,4 +128,5 @@ canvas.bind_all('<KeyPress-X>',button1_pressed)
 canvas.bind_all('<KeyPress-x>',button1_pressed)
 canvas.bind_all('<KeyPress-O>',button2_pressed)
 canvas.bind_all('<KeyPress-o>',button2_pressed)
+canvas.bind_all('<KeyPress-p>',print_items)
 
