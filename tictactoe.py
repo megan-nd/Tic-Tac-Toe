1c2b07e1b841677e4de03e7cc48a7e3c126e56cf
Megan Baig <git@megs.bulldy80.com>
2015-07-11 12:39:55 -0400

    added horizontal lines

--- tictactoe.py
+++ tictactoe.py
@@ -5,3 +5,12 @@ tk.resizable(0,0)
 canvas = Canvas(tk,width=500,height=500,bd=0,highlightthickness=0)
 canvas.pack()
 
+
+def horizontal_line(y,canvas_width,line_length):
+    x1 = canvas_width/2-line_length/2
+    x2 = x1+line_length
+    canvas.create_line(x1,y,x2,y)
+
+horizontal_line(150,500,300)
+horizontal_line(250,500,300)
+
